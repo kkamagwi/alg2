e1887ddfc5453831ac23a0c161b648db11476039
Katya <laptop@Laptops-MacBook-Air.local>
2019-11-22 19:18:06 +0600

    test2

--- tests1.py
+++ tests1.py
@@ -1,4 +1,13 @@
 import unittest
 from alg2.worker import Programist
 
-class Testov(unittest.TestCase)::
+class Testov(unittest.TestCase):
+
+    def test_name(self):
+        oleg = Programist()
+        self.assertEqual(oleg.name, 'Oleg')
+
+
+
+if __name__=="__main__":
+    unittest.main()
